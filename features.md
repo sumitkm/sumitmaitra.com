c601f64da1af3fcc26ecdff80b723e56c05f3e90
Sumit Kumar Maitra <sumitkm@gmail.com>
2016-07-30 00:17:46 +0100

    Added a mvp requirements/features list.

---
+++ features.md
@@ -0,0 +1,38 @@
+# Feature list  
+## Platform
+### UI
+- [ ] Router  
+- [ ] a. Top level routes  
+- [ ] b. 'n' level routes  
+- [ ] c. Test routes using Jasmine/Mocha
+- [ ] Responsive Top Nav component   
+- [ ] Responsive Tab component  
+- [ ] Full screen modal component     
+- [ ] Timeline view component  
+
+### Server  
+- [x] Router with parameters  
+- [x] Controller end points and automatic route registration
+- [ ] Test routes using Mocha
+- [ ] Configuration using nConf  
+- [ ] Implementing [passwordless.net](https://passwordless.net)  
+- [ ] Image resize middleware
+
+## Functionality
+- [ ] Authentication  
+- [ ] Integrate with Kalliope Sync  
+- [ ] Image Geo-tag reading and indexing in DB  
+- [ ] Showing images in timeline view  
+- [ ] Azure search Indexing
+- [ ] Image searching
+- [ ] Online Resume
+- [ ] Live feed of posts from wordpress
+- [ ] Live feed of pinned projects from github
+
+## Devops
+- [ ] Setting up Mailchimp free account
+- [ ] Setting up new Debian VM in Azure
+- [ ] Setting up NGINX
+- [ ] Setting up Letsencrypt
+- [ ] Deploying manually
+- [ ] Setting up Jenkins and setting up CI process
--- main.ts
+++ main.ts
@@ -4,12 +4,49 @@ import * as express from "express";
 import { CrossRouter } from "./app/services/routing/cross-router";
 import { Container } from "./app/di/container";
 
+var passwordless = require('passwordless');
+var MongoStore = require('passwordless-mongostore');
+var email   = require("emailjs");
+
+var smtpServer  = email.server.connect({
+   user:    "yourEmail",
+   password: "yourPwd",
+   host:    "yourSmtp",
+   ssl:     true
+});
+var pathToMongoDb = 'mongodb://192.168.0.57/sumitmaitra';
+app.use(passwordless.sessionSupport());
+app.use(passwordless.acceptToken({ successRedirect: '/'}));
+
+passwordless.init(new MongoStore(pathToMongoDb));
+passwordless.addDelivery(
+    function(tokenToSend, uidToSend, recipient, callback) {
+        var host = 'localhost:3000';
+        smtpServer.send({
+            text:    'Hello!\nAccess your account here: http://'
+            + host + '?token=' + tokenToSend + '&uid='
+            + encodeURIComponent(uidToSend),
+            from:    "yourEmail",
+            to:      recipient,
+            subject: 'Token for ' + host
+        }, function(err, message) {
+            if(err) {
+                console.log(err);
+            }
+            callback(err);
+        });
+});
+
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer(); // for parsing multipart/form-data
 
 var app = express();
 var bodyParser = require('body-parser');
+
+app.use(passwordless.sessionSupport());
+app.use(passwordless.acceptToken({ successRedirect: '/'}));
+
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
